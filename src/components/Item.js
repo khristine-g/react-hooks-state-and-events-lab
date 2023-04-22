ce5348f24f7dfb2bad91d00349d73a47f1c2fb4b
khristine-g <githigekhristine@gmail.com>
2023-04-22 17:03:11 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false); // state variable and setter function
+
+  // function to toggle inCart
+  const toggleInCart = () => {
+    setInCart(!inCart); // toggle the state variable
+  };
+
+  const cartText = inCart ? "Remove From Cart" : "Add to Cart"; // set the button text based on the state variable
+  const itemClass = inCart ? "in-cart" : ""; // set the class based on the state variable
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleInCart}>{cartText}</button> {/* call the toggleInCart function on click */}
     </li>
   );
 }
